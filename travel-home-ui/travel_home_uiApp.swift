d951fe36626c8f2c4f66a96b1ef009d086be7383
amirk3321 <amirk3321@gmail.com>
2023-11-02 14:32:07 +0500

    Initial Commit

---
+++ travel-home-ui/travel_home_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_home_uiApp.swift
+//  travel-home-ui
+//
+//  Created by MA on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct travel_home_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
